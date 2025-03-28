948cec1bca4dcff171a6a2fe4f188dafa28fe0a3
Cauê Amaral <caue.amaral@hotmail.com>
2025-03-28 09:57:00 -0300

    Feat: Getting image from getPokemon()

--- src/cards/Card.tsx
+++ src/cards/Card.tsx
@@ -4,20 +4,33 @@ import { PokemonInterface } from "../interfaces/PokemonInterface"
 
 export function Card({ name }: { name: string }) {
     const [pokemon, setPokemon] = useState<PokemonInterface | undefined>(undefined)
-
+    
     useEffect(() => {
         getPokemon(name)
             .then(response => setPokemon(response.data))
     }, [])
 
+    useEffect(() => {
+        if (! pokemon) return
+
+        console.log(pokemon.sprites.other.dream_world.front_default)
+    }, [pokemon])
+
     return (
         <section className="card">
-            {pokemon ? <h2>{pokemon.name}</h2> : <h2>Pokémon not found</h2>}
-            
             {
                 pokemon
-                    ? <div>Type: {pokemon.types[0].type.name}</div>
-                    : <div>Type: not found</div>
+                    ?
+                        <>
+                            <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} className="" />
+                            <h2>{pokemon.name}</h2>
+                            <div>Type: {pokemon.types[0].type.name}</div>                        
+                        </>
+                    :
+                        <>
+                            <h2>Pokémon not found</h2>
+                            <div>Type: not found</div>
+                        </>
             }
         </section>
     )
--- src/interfaces/PokemonInterface.ts
+++ src/interfaces/PokemonInterface.ts
@@ -2,5 +2,8 @@ export interface PokemonInterface {
     name: string,
     types: {
         type: { name: string }
-    }[]
+    }[],
+    sprites: {
+        other: { dream_world: { front_default: string } }
+    }
 }
