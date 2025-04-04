4e5a0f823916e285e3d8439974f4863bb819932c
Cauê Amaral <caue.amaral@hotmail.com>
2025-04-04 11:12:56 -0300

    Feat: Display all the 151 classic Pokémons

--- src/services/getPokemons.ts
+++ src/services/getPokemons.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function getPokemons() {
-    const endpoint = import.meta.env.VITE_POKEAPI
+    const endpoint = `${import.meta.env.VITE_POKEAPI}/?limit=151`
     const response = axios.get(endpoint)
 
     return response
