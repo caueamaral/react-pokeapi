7ad653516d8ed70c15c90631d58f86ea780470a2
Cauê Amaral <caue.amaral@hotmail.com>
2025-04-08 12:04:11 -0300

    Refactor: Enviroment variable

--- src/services/getPokemonDetails.ts
+++ src/services/getPokemonDetails.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function getPokemonDetails(name: string) {
-    const endpoint = `${import.meta.env.VITE_POKEAPI}/${name}`
+    const endpoint = `${import.meta.env.VITE_POKEAPI}/pokemon/${name}`
     const response = axios.get(endpoint)
 
     return response
--- src/services/getPokemons.ts
+++ src/services/getPokemons.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export function getPokemons() {
-    const endpoint = `${import.meta.env.VITE_POKEAPI}/?limit=151`
+    const endpoint = `${import.meta.env.VITE_POKEAPI}/pokemon/?limit=151`
     const response = axios.get(endpoint)
 
     return response
