6f77cbec579da8da8d39ff4bf95e968def132d34
Cauê Amaral <caue.amaral@hotmail.com>
2025-04-09 17:51:17 -0300

    Feat: Config deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-pokeapi/'
+  base: '/react-pokeapi/',
+  build: {
+    outDir: 'dist'
+  }
 })
